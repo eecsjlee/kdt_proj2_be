c30211c343e6bbea8ed9038eebb7d6a4e35bd98d
eecsjlee <eecsjlee@gmail.com>
2025-02-06 09:39:27 +0900

    docs: 웹소켓 부분 내용 수정

--- src/main/java/com/kdt_proj2_be/config/WebSocketConfig.java
+++ src/main/java/com/kdt_proj2_be/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.kdt_proj2_be.config;
 
 import com.kdt_proj2_be.handler.MyWebSocketHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
