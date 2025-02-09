e5a22cc3348f6f01124cd354ba5cb622f6418f2d
eecsjlee <eecsjlee@gmail.com>
2025-02-09 20:03:24 +0900

    feat: 더미 데이터 추가

---
+++ src/main/resources/dummydata/scrap_type.sql
@@ -0,0 +1,8 @@
+INSERT INTO scrap_type (scrap_type)
+VALUES
+('COPPER'),       -- 구리
+('ALUMINUM'),     -- 알루미늄
+('ZINC'),         -- 아연
+('STAINLESS'),    -- 스테인리스강
+('NICKEL'),       -- 니켈
+('IRON');         -- 철
