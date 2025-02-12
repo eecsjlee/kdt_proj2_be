a1b85385ca135311b7f1cd998dfe6a6fc430b074
eecsjlee <eecsjlee@gmail.com>
2025-02-12 19:20:57 +0900

    style: 어노테이션 누락 수정

--- src/main/java/com/kdt_proj2_be/persistence/ScrapTypeRepository.java
+++ src/main/java/com/kdt_proj2_be/persistence/ScrapTypeRepository.java
@@ -3,9 +3,11 @@ package com.kdt_proj2_be.persistence;
 import com.kdt_proj2_be.domain.ScrapMetalType;
 import com.kdt_proj2_be.domain.ScrapType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ScrapTypeRepository extends JpaRepository<ScrapType, Long> {
 
     Optional<ScrapType> findByScrapType(ScrapMetalType scrapMetalType);
