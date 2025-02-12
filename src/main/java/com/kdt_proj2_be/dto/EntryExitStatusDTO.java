f837f99fe1d2d6da865d51b9ec063ffc46430f05
eecsjlee <eecsjlee@gmail.com>
2025-02-12 14:56:39 +0900

    feat: 이미지 Base64로 전환해서 전달

--- src/main/java/com/kdt_proj2_be/dto/EntryExitStatusDTO.java
+++ src/main/java/com/kdt_proj2_be/dto/EntryExitStatusDTO.java
@@ -2,6 +2,7 @@ package com.kdt_proj2_be.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kdt_proj2_be.domain.Transaction;
+import com.kdt_proj2_be.util.ImageUtil;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,17 +29,30 @@ public class EntryExitStatusDTO {
 
 
     public static EntryExitStatusDTO fromEntity(Transaction transaction) {
+        // base64 인코딩 없이 이미지 파일 이름만 전송하는 경우
+//        return new EntryExitStatusDTO(
+//                transaction.getTransactionStatus().name(),  // ENUM → String 변환
+//                transaction.getCarNumber(),
+//                transaction.getEntryTime(),
+//                transaction.getExitTime(),
+//                transaction.getInImg1(),
+//                transaction.getInImg2(),
+//                transaction.getInImg3(),
+//                transaction.getOutImg1(),
+//                transaction.getOutImg2(),
+//                transaction.getOutImg3()
+//        );
         return new EntryExitStatusDTO(
                 transaction.getTransactionStatus().name(),  // ENUM → String 변환
                 transaction.getCarNumber(),
                 transaction.getEntryTime(),
                 transaction.getExitTime(),
-                transaction.getInImg1(),
-                transaction.getInImg2(),
-                transaction.getInImg3(),
-                transaction.getOutImg1(),
-                transaction.getOutImg2(),
-                transaction.getOutImg3()
+                ImageUtil.encodeImageToBase64(transaction.getInImg1()), // Base64 변환
+                ImageUtil.encodeImageToBase64(transaction.getInImg2()),
+                ImageUtil.encodeImageToBase64(transaction.getInImg3()),
+                ImageUtil.encodeImageToBase64(transaction.getOutImg1()),
+                ImageUtil.encodeImageToBase64(transaction.getOutImg2()),
+                ImageUtil.encodeImageToBase64(transaction.getOutImg3())
         );
     }
 }
