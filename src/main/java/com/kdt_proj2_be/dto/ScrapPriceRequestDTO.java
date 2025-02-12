7f13ddeabddcfdac727be6c546d2307547832001
eecsjlee <eecsjlee@gmail.com>
2025-02-12 15:01:51 +0900

    comment: 주석 정리

--- src/main/java/com/kdt_proj2_be/dto/ScrapPriceRequestDTO.java
+++ src/main/java/com/kdt_proj2_be/dto/ScrapPriceRequestDTO.java
@@ -16,7 +16,7 @@ import java.util.Map;
 @NoArgsConstructor
 public class ScrapPriceRequestDTO {
 
-    private LocalDate effectiveDate; // 날짜만 입력받고, 시간은 00:00:00으로 처리
+    private LocalDate effectiveDate; // 날짜만 입력받음 나중에 시간 00:00:00을 추가하여 LocalDateTime 형태로 변환
 
     @JsonDeserialize(keyUsing = ScrapMetalTypeDeserializer.class) // ✅ ENUM 키 변환 추가
     private Map<ScrapMetalType, BigDecimal> prices; // 여러 고철 가격을 받기 위해 Map 사용
